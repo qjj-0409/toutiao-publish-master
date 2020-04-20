2c49914cd1adb9534e4efd3e75e65e1b3155fbe1
qjj-0409 <qi18790396124@163.com>
2020-04-20 15:09:25 +0800

    调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+// 项目的启动入口
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载element组件库
+import ElementUI from 'element-ui'
+
+// 加载element组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入全局样式文件
+import './styles/index.less'
+
+// 全局注册element组件库
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
