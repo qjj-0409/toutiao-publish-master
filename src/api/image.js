c1137e13612b5bc99cf32913614788186f6fb94c
qjj-0409 <qi18790396124@163.com>
2020-04-27 17:26:41 +0800

    素材管理-分页列表&收藏删除素材

--- src/api/image.js
+++ src/api/image.js
@@ -21,3 +21,22 @@ export const getImages = params => {
     params
   })
 }
+
+// 收藏图片素材接口
+export const isCollectImage = (collect, id) => {
+  return request({
+    method: 'PUT',
+    url: `/mp/v1_0/user/images/${id}`,
+    data: {
+      collect
+    }
+  })
+}
+
+// 收藏图片素材接口
+export const delImage = (id) => {
+  return request({
+    method: 'DELETE',
+    url: `/mp/v1_0/user/images/${id}`
+  })
+}
--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -2,12 +2,16 @@
   <div class="image-container">
     <el-card class="box-card">
       <div slot="header" class="clearfix">
-        <span>卡片名称</span>
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>素材管理</el-breadcrumb-item>
+        </el-breadcrumb>
       </div>
       <div style="padding-bottom:20px" class="image-flex">
         <el-radio-group
          v-model="collect"
          size="medium"
+         :disabled="loading"
          @change="loadImages()"
         >
           <el-radio-button
@@ -20,20 +24,45 @@
         <el-button
          type="success"
          size="medium"
+         :disabled="loading"
          @click="dialogVisible = true"
         >添加素材</el-button>
       </div>
-      <el-row :gutter="20" v-model="images">
+      <el-row :gutter="20" v-model="images" v-loading="loading">
         <el-col :xs="12" :sm="6" :md="6" :lg="4"
+          class="img-box"
           v-for="(img, index) in images"
           :key="index"
         >
           <el-image
           style="height: 100px"
           :src="img.url"
-          fit="cover"></el-image>
+          fit="cover"
+          ></el-image>
+          <div class="img-mask">
+            <span
+              class="el-icon-star-off icon-xing"
+              :style="img.is_collected ? 'color:red' : 'color:white'"
+              @click="isCollect(!img.is_collected, img.id)"
+            ></span>
+            <span
+              class="el-icon-delete icon-del"
+              @click="delImage(img.id)"
+            ></span>
+          </div>
         </el-col>
       </el-row>
+      <el-pagination
+        class="image-pag"
+        background
+        layout="prev, pager, next"
+        :total="totalCount"
+        :page-size="pageSize"
+        :current-page.sync="currentPage"
+        :disabled="loading"
+        @current-change="onCurrentChange"
+      >
+      </el-pagination>
     </el-card>
 
     <el-dialog
@@ -60,7 +89,11 @@
 
 <script>
 // 导入图片相关的接口
-import { getImages } from '@/api/image.js'
+import {
+  getImages,
+  isCollectImage,
+  delImage
+} from '@/api/image.js'
 export default {
   name: 'ImageIndex',
   props: {},
@@ -73,7 +106,12 @@ export default {
       dialogVisible: false, // 是否显示对话框
       uploadHeaders: {
         Authorization: `Bearer ${user.token}`
-      }
+      },
+      pageSize: 12, // 每页显示条数
+      totalCount: 0, // 总素材条数
+      currentPage: 1, // 当前页
+      loading: false, // 是否显示加载中
+      imageCollect: false // 当前图片是否收藏
     }
   },
   computed: {},
@@ -82,18 +120,61 @@ export default {
     this.loadImages()
   },
   methods: {
-    loadImages (collect = false) {
+    loadImages (page, collect = false) {
+      // 加载中
+      this.loading = true
       getImages({
+        page,
+        per_page: this.pageSize,
         collect: this.collect
       }).then(res => {
-        this.images = res.data.data.results
+        const { results, total_count: totalCount } = res.data.data
+        this.images = results
+        this.totalCount = totalCount
+        // 关闭加载中
+        this.loading = false
       })
     },
     onUploadSuccess () {
       // 关闭对话框
       this.dialogVisible = false
       // 重新渲染素材列表
-      this.loadImages(false)
+      this.loadImages(this.currentPage, false)
+    },
+    onCurrentChange (page) {
+      // 当页码改变的时候重新渲染素材列表
+      this.loadImages(page)
+    },
+    isCollect (isCollected, imageId) {
+      isCollectImage(isCollected, imageId).then(res => {
+        console.log(res)
+        // 更改收藏成功，刷新页面
+        this.loadImages(this.currentPage)
+      })
+    },
+    delImage (imageId) {
+      this.$confirm('确定删除吗？', '删除提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        delImage(imageId).then(res => {
+          this.$message({
+            message: res.message,
+            type: 'success'
+          })
+          // 删除成功，更新素材列表
+          this.loadImages(this.currentPage)
+        }).catch(err => {
+          console.log('错误：' + err)
+          this.$message.error('删除失败')
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     }
   },
   mounted () {}
@@ -103,5 +184,27 @@ export default {
 .image-flex {
   display: flex;
   justify-content: space-between;
+}
+.img-box {
+  position: relative;
+  margin-bottom: 20px;
+}
+.img-mask {
+  position: absolute;
+  bottom: 4px;
+  margin: 0 auto;
+  height: 28px;
+  line-height: 28px;
+  background: rgba(0,0,0,.3);
+  width: 90%;
+  text-align: center;
+  .icon-xing {
+    margin: 0 15px;
+    color: #fff;
+  }
+  .icon-del {
+    margin: 0 15px;
+    color: #fff;
+  }
 }
 </style>
