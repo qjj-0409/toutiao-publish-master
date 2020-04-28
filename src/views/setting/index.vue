71b372a037e1bae90015e514cf1fd852a5fe23a4
qjj-0409 <qi18790396124@163.com>
2020-04-28 18:46:16 +0800

    个人设置-上传头像简单预览

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -10,8 +10,8 @@
       </div>
       <!-- /面包屑导航 -->
       <el-row>
+        <!-- 表单区域 -->
         <el-col :span="10">
-          <!-- 表单区域 -->
           <el-form ref="form" :model="user" label-width="80px">
             <el-form-item label="编号:">
               {{user.id}}
@@ -32,21 +32,47 @@
               <el-button type="primary" @click="onSubmit">保存设置</el-button>
             </el-form-item>
           </el-form>
-          <!-- /表单区域 -->
         </el-col>
+        <!-- /表单区域 -->
+        <!-- 头像区域 -->
         <el-col :span="8" :offset="6">
-          <!-- 头像区域 -->
-          <el-avatar
-            shape="square"
-            :size="150"
-            fit="cover"
-            :src="user.photo"
-          ></el-avatar>
+          <label for="file">
+            <el-avatar
+              shape="square"
+              :size="150"
+              fit="cover"
+              :src="user.photo"
+            ></el-avatar>
           <p>点击修改头像</p>
-          <!-- /头像区域 -->
+          </label>
+          <input
+            id="file"
+            type="file"
+            hidden
+            ref="file"
+            @change="onFileChange"
+          >
         </el-col>
+        <!-- /头像区域 -->
       </el-row>
     </el-card>
+    <!-- 弹出层 -->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      append-to-body
+      width="30%"
+    >
+      <img
+        class="previewImage"
+        :src="previewImage"
+        alt="">
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- /弹出层 -->
   </div>
 </template>
 
@@ -75,7 +101,9 @@ export default {
         photo: '', // 头像
         email: '', // 邮箱
         mobile: '' // 手机号
-      }
+      },
+      dialogVisible: false, // 是否显示弹出层对话框
+      previewImage: '' // 预览图片路径
     }
   },
   computed: {},
@@ -90,13 +118,26 @@ export default {
     // 获取用户个人信息
     uploadUserInfo () {
       getUserProfile().then(res => {
-        console.log(res)
         this.user = res.data.data
       })
+    },
+    // 上传图片改变
+    onFileChange () {
+      const file = this.$refs.file
+      // console.dir(file)
+      const blobData = window.URL.createObjectURL(file.files[0])
+      this.previewImage = blobData
+      // 显示弹出层
+      this.dialogVisible = true
+      // 解决选择相同文件不触发change事件问题
+      this.$refs.file.value = ''
     }
   },
   mounted () {}
 }
 </script>
 <style lang='less' scoped>
+.previewImage {
+  width: 60%;
+}
 </style>
