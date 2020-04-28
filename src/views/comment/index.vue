7844bb78b1d4a3e60d8a245e75e97a3fcf8f3bda
qjj-0409 <qi18790396124@163.com>
2020-04-28 17:32:38 +0800

    评论管理-分页功能实现

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="comment-container">
     <el-card class="box-card">
+      <!-- 面包屑导航 -->
       <div slot="header" class="clearfix">
         <el-breadcrumb separator="/">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>评论管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- /面包屑导航 -->
+      <!-- 评论列表 -->
       <el-table
         :data="comments"
+        v-loading="loading"
         style="width: 100%">
         <el-table-column
           prop="title"
@@ -26,7 +30,7 @@
         </el-table-column>
         <el-table-column
           prop="comment_status"
-          label="状态"
+          label="评论状态"
         >
           <template slot-scope="scope">
            {{scope.row.comment_status ? '正常' : '关闭'}}
@@ -46,6 +50,22 @@
           </template>
         </el-table-column>
       </el-table>
+      <!-- /评论列表 -->
+      <!-- 分页组件 -->
+      <el-pagination
+        class="page"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        background
+        :current-page.sync="page"
+        :page-sizes="[10, 20, 30, 50]"
+        :page-size.sync="perPage"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="totalCount"
+        :disabled="loading"
+      >
+      </el-pagination>
+      <!-- /分页组件 -->
     </el-card>
   </div>
 </template>
@@ -61,65 +81,68 @@ export default {
   components: {},
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
       comments: [], // 评论列表
       totalCount: 0, // 总评论数
       page: 1, // 当前页
-      perPage: 20 // 每页显示条数
+      perPage: 10, // 每页显示条数
+      loading: false
     }
   },
   computed: {},
   watch: {},
   created () {
-    this.onloadComments()
+    this.onloadComments(1)
   },
   methods: {
-    onloadComments () {
+    onloadComments (page = 1) {
+      // 开启加载中
+      this.loading = true
+      this.page = page
       getArticle({
-        page: 1,
+        page,
         per_page: this.perPage,
         response_type: 'comment'
       }).then(res => {
-        const { results } = res.data.data
+        console.log(res)
+        const { results, total_count: totalCount } = res.data.data
         results.forEach(article => {
           article.loading = false
         })
         this.comments = res.data.data.results
+        this.totalCount = totalCount
+        // 关闭加载中
+        this.loading = false
       }).catch(err => {
         console.log('错误：' + err)
+        // 关闭加载中
+        this.loading = false
       })
     },
     changeStatus (row) {
       // 打开loading，禁用开关
       row.loading = true
+      // 请求提交修改
       updateCommentStatus(row.id.toString(), row.comment_status).then(res => {
         this.$message({
           type: 'success',
-          message: row.comment_status ? '打开评论功能' : '关闭评论功能'
+          message: row.comment_status ? '开启评论功能' : '关闭评论功能'
         })
         // 关闭loading，启用开关
         row.loading = false
       })
+    },
+    handleSizeChange () {
+      this.onloadComments()
+    },
+    handleCurrentChange () {
+      this.onloadComments(this.page)
     }
   },
   mounted () {}
 }
 </script>
 <style lang='less' scoped>
+.page {
+  margin-top: 20px;
+}
 </style>
